e8e67c7eaaca16a71cd6c0a140b55e473cee885b
Benedict <chensxb97@gmail.com>
2024-08-16 18:06:53 +0800

    update introduction

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,10 @@ export default function Page() {
         hi there, I'm Benedict 👨‍💻
       </h1>
       <p className="mb-4">
-        I currently work as a full-stack software engineer in Visa's Observability team, where I design and implement new features and applications to support infrastructure monitoring and logging.
+        I am currently a full-stack software engineer in Visa's Observability team, where I design and implement new features and solutions to support infrastructure monitoring and logging.
+      </p>
+      <p className="mb-4">
+        I mostly work using React, Golang and Python.
       </p>
       <p className="mb-4">
         When I am not writing code, you’ll often catch me climbing, experimenting new coffee recipes or planning my next vacation.
