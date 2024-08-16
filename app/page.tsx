8f89776ab9117e83f831cd3425b2941b665444c6
Benedict <chensxb97@gmail.com>
2024-08-16 17:50:42 +0800

    Removed headline

--- app/page.tsx
+++ app/page.tsx
@@ -5,10 +5,7 @@ export default function Page() {
         hi there, I'm Benedict 👨‍💻
       </h1>
       <p className="mb-4">
-        A software engineer, rock-climber and coffee-lover.
-      </p>
-      <p className="mb-4">
-        I currently work as a full-stack developer in Visa's Observability team, where I design and develop new features and applications to support infrastructure monitoring and logging.
+        I currently work as a full-stack software engineer in Visa's Observability team, where I design and implement new features and applications to support infrastructure monitoring and logging.
       </p>
       <p className="mb-4">
         When I am not writing code, you’ll often catch me climbing, experimenting new coffee recipes or planning my next vacation.
