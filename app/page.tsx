da859f1d41d54254b8066cbb0226686c620d3c2a
Benedict <chensxb97@gmail.com>
2024-08-17 10:22:55 +0800

    Updated profile

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link'
+
 export default function Page() {
   return (
     <section>
@@ -5,14 +7,37 @@ export default function Page() {
         hi there, I'm Benedict 👨‍💻
       </h1>
       <p className="mb-4">
-        I am currently a full-stack software engineer in Visa's Observability team, where I design and implement new features and solutions to support infrastructure monitoring and logging.
+        I am a software engineer, rock-climber and coffee-lover.
       </p>
       <p className="mb-4">
+        Today, I work as a full-stack developer at <a rel="noopener noreferrer" target='_blank' href='https://www.visa.com.sg/' style={{ color: '#91e8ff' }}>
+          Visa Inc</a>.
+        As part of the Observability team, I design and implement new solutions to support infrastructure monitoring and logging.
         I mostly work using React, Golang and Python.
       </p>
       <p className="mb-4">
-        When I am not writing code, you’ll often catch me climbing, experimenting new coffee recipes or planning my next vacation.
-      </p>
+        The best way to reach out to me is through <a
+          rel="noopener noreferrer"
+          target="_blank"
+          href="https://www.linkedin.com/in/benedict-chen-1a8024189/"
+          style={{ color: '#91e8ff' }}>
+          LinkedIn
+        </a>.
+        <br />
+        Otherwise, feel free to also explore my  <Link
+          key="/work"
+          href="/work"
+          style={{ color: '#FFC0CB' }}
+        >
+          work
+        </Link> and <Link
+          key="/blog"
+          href="/blog"
+          style={{ color: '#FFC0CB' }}
+        >
+          writings
+        </Link>.
+      </p >
     </section >
   )
 }
