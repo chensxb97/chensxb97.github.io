e9c6a4fd80a00d95c622d3cfa609c1150c8186ac
Benedict <chensxb97@gmail.com>
2024-08-17 15:30:18 +0800

    updated default title

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ export const metadata: Metadata = {
     icon: '/icon.png',
   },
   title: {
-    default: 'Home | Benedict Chen',
+    default: 'Benedict Chen',
     template: '%s | Benedict Chen',
   },
   description: 'Benedict Chen\'s Portfolio.',
