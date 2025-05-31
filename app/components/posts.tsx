afaf562d8d5b8e217df489147a77c5dc5c4b4d93
Benedict <chensxb97@gmail.com>
2025-05-31 11:12:37 +0800

    revamp blog layout

--- app/components/posts.tsx
+++ app/components/posts.tsx
@@ -21,13 +21,18 @@ export function BlogPosts() {
             className="flex flex-col space-y-1 mb-4"
             href={`/blog/${post.slug}`}
           >
-            <div className="w-full flex flex-col md:flex-row space-x-0 md:space-x-3">
-              <p className="text-sm dark:text-neutral-400 mt-1">
+            <div className="w-full flex flex-col md:flex-row md:space-x-3 space-y-1 md:space-y-0">
+              <p className="md:w-1/4 text-sm dark:text-neutral-400 mt-1">
                 {formatDate(post.metadata.publishedAt, false)}
               </p>
-              <p className="text-md dark:text-neutral-100">
-                {post.metadata.title}
-              </p>
+              <div className="w-full">
+                <p className="text-l font-medium text-neutral-900 dark:text-neutral-100">
+                  {post.metadata.title}
+                </p>
+                <p className="text-xs text-neutral-700 dark:text-neutral-300">
+                  {post.metadata.summary}
+                </p>
+              </div>
             </div>
           </Link>
         ))}
