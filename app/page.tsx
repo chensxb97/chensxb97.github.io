a4d9527d1ab51e6c46343223026971382f444207
Benedict <chensxb97@gmail.com>
2025-06-06 15:44:12 +0800

    grammar

--- app/page.tsx
+++ app/page.tsx
@@ -39,7 +39,7 @@ export default function Page() {
             href="https://github.com/chensxb97"
             className="link">
             side projects
-          </a> and, occasionally, document personal experiences, thoughts and learnings in my <Link
+          </a> and occasionally, document personal experiences, thoughts and learnings in my <Link
             key="/blog"
             href="/blog"
             className="link"
